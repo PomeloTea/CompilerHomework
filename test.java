cbf87d8819bab8bf8cd455e66fa1741f85327a18
Kerr <linke0113@gmail.com>
2014-06-09 13:24:35 +0800

    no message

--- test.java
+++ test.java
@@ -2,16 +2,7 @@ public static class MyArrayList {
 
 	
 	boolean add(int index, int element) {
-		if (index > this.size || index < 0) {
-			return false;
-		} else {
-			ensureCapacity(++this.size);
-			for (int i = this.size - 1; i > index; i--) {
-				this.elementData[i] = this.elementData[i - 1];
-			}
-			this.elementData[index] = element;
-			return true;
-		}
+		this.elementData[this.size++] = element;
 	}
 
 }
